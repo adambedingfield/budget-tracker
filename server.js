6897baa15a3191b5a5928c79c590bbc2aff21c74
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-06-27 20:26:23 -0700

    deploying

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 
 // routes
 app.use(require("./routes/api.js"));
